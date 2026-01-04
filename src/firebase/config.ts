dd1a244e24d7ac3c62cc4b818ab62fca75ad4421
Ojaswi Dwivedi <ojaswi.dwivedi78@gmail.com>
2026-01-04 14:49:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6467765650-d1885",
+  "appId": "1:244564830402:web:11e6a1006836f4cd57335b",
+  "apiKey": "AIzaSyAzhsrwyUZTIaLNvbPO9N7fqBF3QOa0mAQ",
+  "authDomain": "studio-6467765650-d1885.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "244564830402"
+};
